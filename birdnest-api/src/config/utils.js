d05d5bdcd2416870e006d37d08e230d774afc3ec
tomi <tomiwest@hotmail.com>
2023-01-09 11:41:07 +0200

    fetch data from api and create drone objects

---
+++ birdnest-api/src/config/utils.js
@@ -0,0 +1,54 @@
+import xml2js from 'xml2js'
+
+export const parseXmlToJsonObject = (body) => {
+  let drones
+  xml2js.parseString(body, { trim: true }, (err, result) => {
+    if (err) {
+      throw err
+    }
+    const obj = JSON.parse(JSON.stringify(result, null, 4))
+    const snapshotTimestamp = obj.report.capture[0].$.snapshotTimestamp
+
+    drones = obj.report.capture[0].drone.map((drone) => {
+      return parseDroneFields(drone, snapshotTimestamp)
+    })
+    //console.log(util.inspect(drones, false, null))
+  })
+  return drones
+}
+
+/**
+ *
+ * @param {*} drone
+ * @returns A drone object without properties as arrays
+ */
+export const parseDroneFields = (drone, snapshotTimestamp) => {
+  const droneObjectParsed = {
+    serialNumber: drone.serialNumber[0],
+    model: drone.model[0],
+    manufacturer: drone.manufacturer[0],
+    mac: drone.mac[0],
+    ipv4: drone.ipv4[0],
+    ipv6: drone.ipv6[0],
+    firmware: drone.firmware[0],
+    positionY: drone.positionY[0],
+    positionX: drone.positionX[0],
+    altitude: drone.altitude[0],
+    snapshotTimestamp: snapshotTimestamp,
+    confirmedDistance: getDistanceFromCenter(
+      drone.positionX[0],
+      drone.positionY[0]
+    ),
+  }
+  return droneObjectParsed
+}
+export const getDistanceFromCenter = (x, y) => {
+  const centerX = 250000
+  const centerY = 250000
+
+  let distanceX = Math.abs(x - centerX)
+  let distanceY = Math.abs(y - centerY)
+  let distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY)
+
+  return distance / 1000
+}
--- birdnest-api/src/graphql/queries/SensorApi.js
+++ birdnest-api/src/graphql/queries/SensorApi.js
@@ -1,7 +1,23 @@
 import { AuthenticationError, gql } from 'apollo-server'
+import { GraphQLError } from 'graphql'
+
+import Bird from '../../models/Bird.js'
+
+import fetch from 'node-fetch'
 import dotenv from 'dotenv'
+import { parseXmlToJsonObject } from '../../config/utils.js'
+
 dotenv.config()
-const API_SECRET = process.env.API_SECRET
+
+const API_KEY = process.env.API_KEY
+
+const defaultBird = 'Monadikuikka'
+
+const getResponseAsDroneObjects = async (url) => {
+  const nestdata = await fetch(`http://${url}`)
+  const body = await nestdata.text()
+  return parseXmlToJsonObject(body)
+}
 
 export const typeDefs = gql`
   extend type Query {
@@ -11,10 +27,20 @@ export const typeDefs = gql`
 export const resolvers = {
   Query: {
     getSensorData: async (_, { apiKey }) => {
-      if (apiKey !== API_SECRET) {
+      if (apiKey !== API_KEY) {
         throw new AuthenticationError('Invalid Api key')
       }
-
+      //For the sake of the task hardcode to find "Monadinkuikka"
+      const birdExists = await Bird.findOne({ name: defaultBird }).populate(
+        'protectedNests'
+      )
+      if (!birdExists) {
+        throw new GraphQLError(`Bird ${defaultBird} does not exist`)
+      }
+      //Get the first nest
+      const [firstNest] = birdExists.protectedNests
+      const drones = await getResponseAsDroneObjects(firstNest.url)
+      console.log(drones)
       return 'Access to call api granted'
     },
   },
