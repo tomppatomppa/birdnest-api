ca5e29b453c5405407dee00f7f9304bf8c3929a2
tomi <tomiwest@hotmail.com>
2023-02-13 18:04:21 +0200

    save

--- birdnest-api/src/index.js
+++ birdnest-api/src/index.js
@@ -3,6 +3,9 @@ import dotenv from 'dotenv'
 import { sendRequestToGraphqlEndpoint } from './config/utils.js'
 import { InitDatabase } from './config/db.js'
 import { createApolloServer } from './config/apolloServer.js'
+import Nest from './models/Nest.js'
+import Pilot from './models/Pilot.js'
+import Drone from './models/Drone.js'
 
 dotenv.config()
 
@@ -11,6 +14,15 @@ InitDatabase() //Initialize mongodb
 createApolloServer() //create and start apollo server
 
 //Send request every 5 seconds
-setInterval(() => {
-  sendRequestToGraphqlEndpoint()
-}, 5000)
+// setInterval(() => {
+//   sendRequestToGraphqlEndpoint()
+// }, 5000)
+
+// await Pilot.deleteMany({})
+// await Drone.deleteMany({})
+// const found = await Nest.findOne({
+//   url: 'assignments.reaktor.com/birdnest/drones',
+// })
+// found.violations = []
+// await found.save()
+// console.log(found)
